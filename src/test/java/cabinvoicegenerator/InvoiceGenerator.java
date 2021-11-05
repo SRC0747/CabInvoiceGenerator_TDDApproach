a6dd89d6515176370b7bd0cef641df666419100d
SRC0747 <RjSrc@0747>
2021-11-05 22:28:31 +0530

    Refactored: Calculate Total Fare and Minimum Fare by creating InvoiceGenerator which take Distance and time correspondingly.

--- src/test/java/cabinvoicegenerator/InvoiceGenerator.java
+++ src/test/java/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,10 +1,24 @@
 package cabinvoicegenerator;
 
+/**
+ * InvoiceGenerator is used to calculate Total Fare by taking distance and time and also compare with the Minimum fare.
+ *
+ * @author Sampriti Roy Chowdhury
+ * @version 0.0.1
+ * @since 5-11-2021
+ */
+
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KILLOMETER = 10;
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
 
+    /**
+     * calculateFare method calculate totalFare and Minimum Fare taking distance and time.
+     * @param distance
+     * @param time
+     * @return totalFare and Minimum Fare
+     */
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILLOMETER + time * COST_PER_TIME;
         if(totalFare < MINIMUM_FARE)
