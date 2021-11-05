26f27747a6244f4e3a434210e2db0fc011f39d98
SRC0747 <RjSrc@0747>
2021-11-05 22:53:51 +0530

    Calculate total and minimum fare of Multiple Rides Creating Ride Class to access Multiple Rides Service and also make connection with InvoiceGenerator.

--- src/test/java/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -19,7 +19,7 @@ class CabInvoiceGeneratorTest {
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double distance = 0.1;
+        double distance = 0.4;
         int time = 1;
         double minimumFare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5, minimumFare, 0.0);
@@ -29,9 +29,9 @@ class CabInvoiceGeneratorTest {
     public void givenMultipleRides_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = { new Ride(2.0, 5),
-                new Ride(0.1, 1),
+                new Ride(0.4, 1),
         };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(30, fare, 0.0);
+        double totalFare = invoiceGenerator.calculateFareMultipleRides(rides);
+        Assertions.assertEquals(5.0, totalFare, 0.0);
     }
 }
